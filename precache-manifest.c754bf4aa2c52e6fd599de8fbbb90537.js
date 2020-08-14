a9c4eaef09a90da8c081ec3e9489bc4616e45ec1
GSRdevelopment <gsrdevelopment.contact@gmail.com>
2020-08-14 13:09:56 -0400

    initial files

---
+++ precache-manifest.c754bf4aa2c52e6fd599de8fbbb90537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfad2847c0a88f37042f0a6264b0522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15179f28dc26d37acb80",
+    "url": "/static/css/main.9e25007d.chunk.css"
+  },
+  {
+    "revision": "ddf6dc12843b86de56a5",
+    "url": "/static/js/2.43dd0c9c.chunk.js"
+  },
+  {
+    "revision": "3e6933cf6bd44f8f91c7aa725d75c668",
+    "url": "/static/js/2.43dd0c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15179f28dc26d37acb80",
+    "url": "/static/js/main.6d675361.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
